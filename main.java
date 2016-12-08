24060d7985517ba8a5dba6ce6282a27cd04f6677
GaoleiBai <937294944@qq.com>
2016-12-08 13:41:45 +0800

    Create main.java

---
+++ main.java
@@ -0,0 +1,26 @@
+
+
+public class Data
+{
+	public static void main(String arg[])
+	{
+		int a = 100;
+		short b = 12;
+		long c = 123;
+		
+		char e = 'a';
+		char f = 'жа';
+		
+		byte g = 123;
+		
+		boolean b1 = true;
+		boolean b2 = false;
+		
+		float f1 = (float)0.12;
+		
+		double d1 = 0.12;
+		
+		System.out.println("e = " + e);
+		System.out.println("f = " + f);
+	}
+}
